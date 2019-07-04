a4a7d4121cc6d3eb84116890e50645040f6a9bab
Shounak.Joshi <shounak.joshi@harbingergroup.com>
2019-07-04 14:50:25 +0530

    using list in react -- code added.

--- react_tutorial/src/App.js
+++ react_tutorial/src/App.js
@@ -1,58 +1,125 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-// class App extends Component {
-const app = props => {
-  const [personState, setPersonsState] = useState(
-    {
-      persons: [
-        {name: 'Shounak', age: 23},
-        {name: 'Devendu', age: 34},
-        {name: 'Vinayak', age: 25},
-      ]
-    }    
-  );
+class App extends Component {
 
-  const [otherState, setOtherState] = useState('Other state here!');
+  state = {
+    persons: [
+      {id: 'asasff', name: 'Shounak', age: 23},
+      {id: 'asdsaaff', name: 'Devendu', age: 34},
+      {id: 'sfdsgdhh', name: 'Vinayak', age: 25},
+    ],
+    otherState: 'asach watla mhanun state dili :P',
+    showPersons: false
+  }
+  // const [personState, setPersonsState] = useState(
+  //   {
+  //     persons: [
+  //       {name: 'Shounak', age: 23},
+  //       {name: 'Devendu', age: 34},
+  //       {name: 'Vinayak', age: 25},
+  //     ]
+  //   }    
+  // );
 
-  console.log(personState, otherState);
+  // const [otherState, setOtherState] = useState('Other state here!');
+
+  // console.log(personState, otherState);
   
   // If we use this kind of function based state handling then it 
   // does not merge the state but replaces old state with new one.
-  const switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log('Button clicked');
     // DONT DO THIS: this.state.persons[0].name='Malhar'
-    setPersonsState(
+    this.setState(
       {persons: [
-        {name: 'Malhar', age: 23},
+        {name: newName, age: 23},
         {name: 'Devendu', age: 33},
         {name: 'Vinayak', age: 26},
       ]}
     )
   };
 
-    return ( 
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
-        <Person name={personState.persons[1].name} age={personState.persons[1].age}/>
-        <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
-      </div>
-    );
+  nameChangeHandler = (event, id) => {
+    
+    const personIndex = this.state.persons.findIndex(p =>{
+      return p.id === id;
+    });
 
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi! I am new React App!'))
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    
+    this.setState({persons: persons});
   }
 
-export default app;
+  deletePersonHandler = (personIndex) => {
+      // const persons = this.state.persons;
+      const persons = [...this.state.persons];
+      persons.splice(personIndex, 1);
+      this.setState({persons: persons});
+  }
+
+  togglePersonView = () => {
+    const doesShow = this.state.showPersons; 
+    this.setState({
+      showPersons: !doesShow
+    });
+  }
+
+  style = {
+    backgroundColor: 'white',
+    font: 'comic sans ms',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
+
+
+    render(){
+
+      let persons = null;
+
+      if (this.state.showPersons){
+        persons = (
+          <div>
+            {this.state.persons.map((person, index) => {
+              return <Person 
+              click={() => this.deletePersonHandler(index)}
+              name={person.name} 
+              age={person.age} 
+              key={person.id}
+              changed={(event) => this.nameChangeHandler(event, person.id)}
+              />
+            })}
+          </div>
+        );
+      }
+
+      return ( 
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <button style={this.style} onClick={this.togglePersonView}>Switch Name</button>
+          {persons}
+        </div>
+      );
+    }
+  }
+
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi! I am new React App!'))
+
+export default App;
 
 // state = {
 //   persons: [
